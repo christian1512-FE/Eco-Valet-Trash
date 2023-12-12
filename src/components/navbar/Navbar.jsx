a28c9815f024e6b22ca2b5a1852845fb5d697fed
Your Name <christianmendez@Christians-MBP.attlocal.net>
2023-12-12 10:28:37 -0600

    added files and folders

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./nav.css";
+
+function Navbar() {
+  return (
+    <div>
+      <h1>navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from "./header/Header";
+export { default as Services } from "./services/Services";
+export { default as Contact } from "./contact/Contact";
+export { default as About } from "./about/About";
