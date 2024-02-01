01a5bb5a75553ec679ab4483f0fa2b544f3225bf
Your Name <christianmendez@Christians-MBP.attlocal.net>
2024-02-01 14:20:34 -0600

    added contact & footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React from "react";
+import { FaLinkedin, FaInstagram } from "react-icons/fa";
+import ecologo from "../../assets/eco-valet-logo-copy.png";
+
 import "./footer.css";
 
-function Footer() {
+const Footer = () => {
+  const linkedin = "https://www.linkedin.com/in/eco-valet-trash/";
+  const insta = "https://www.instagram.com/ecovalettrash/";
+
   return (
-    <div>
-      {/* <h1>Footer</h1> */}
+    <div className="footer-container">
+      <div className="footer-logo">
+      <a href="#nav">
+        <img src={ecologo} alt="logo" /> 
+        </a>
+        </div>
+      <div className="links">
+        <a href={linkedin}>
+          <FaLinkedin />
+        </a>
+        <a href={insta}>
+          <FaInstagram />
+        </a>
+      </div>
+      {/* <span class="phone">800.ECO.1826</span> */}
+      <div className="footer-copy-right">
+       Copyright © {new Date().getFullYear()} <strong> Eco Valet Trash</strong>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
+
+//ADD phone number
+// address
+// logo
+// social media
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+import testingimage from "../../assets/testing-image.png";
 import "./contact.css";
 
 function Contact() {
+  const form = useRef();
+  const [isSending, setIsSending] = useState(false);
+
+  
+  const sendEmail = (e) => {
+      e.preventDefault();
+      setIsSending(true);
+
+      emailjs.sendForm('service_q2hv0sr', 'template_m4shwwt', form.current, '1dk2hbA7g7OV-wJn5')
+        .then((result) => {
+            console.log(result.text);
+            // reset 
+            form.current.reset();
+            setIsSending(false);
+
+        }, (error) => {
+            console.log(error.text);
+            setIsSending(false);
+
+        });
+    };
+
   return (
-    <div>
-      {/* <h1>Contact</h1> */}
+    <div className="contact-container background-color" id="contact">
+      <div className="contact-content">
+        <form className="contact-form" ref={form} onSubmit={sendEmail}>
+        <h1>Contact Us</h1>
+        <p>Join our team as we strive to redefine the standard for curbside trash and recycling collection in residential communities, setting new benchmarks as the nation's leading provider.</p>
+          <input type="text" name="Full-name" placeholder="Full Name" required />
+          <input type="text" name="email" placeholder="Email*" required />
+          <input type="text" name="phone" placeholder="Phone Number*" required />
+          <input type="text" name="community-name" placeholder="Community Name*" required />
+
+          <select name="number-of-units" required>
+            <option value="">Number of Units</option>
+            <option value="50-99 units">50-99 units</option>
+            <option value="100-149 units">100-149 units</option>
+            <option value="150-199 units">150-199 units</option>
+            <option value="200+ units">200+ units</option>
+          </select>
+
+          <textarea rows="5" cols="50" name="message" placeholder="Your message here" required></textarea>
+          <button type="submit" className="btn-contact" name="submit" disabled={isSending}>
+            {isSending ? 'Message Sent' : 'Send Message'}
+          </button>
+        </form>
+      </div>
+        <div className="contact-image">
+        <img src={testingimage} alt="trash-steps" />
+        </div>
     </div>
   )
 }
